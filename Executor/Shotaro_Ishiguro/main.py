d24303e4b6a6fe0bfb41422c585c5cb2c10a5b1c
Shotaro Ishiguro <tennisshow20011218@gmail.com>
2025-05-19 11:44:36 +0900

    引数を試す

--- Executor/Shotaro_Ishiguro/main.py
+++ Executor/Shotaro_Ishiguro/main.py
@@ -1,8 +1,17 @@
 import time
+import argparse
 
-for i in range(10):
+parser = argparse.ArgumentParser(description='このプログラムの説明')    # 2. パーサを作る
+
+parser.add_argument('time_for_run')
+parser.add_argument('-n', '--myname')
+
+args = parser.parse_args()    # 3. 引数を取得する
+
+for i in range(args.time_for_run):
     time.sleep(1)
     print(i)
+    print("name: ", args.myname)
     # print('\a')
 
 print("実行完了")
